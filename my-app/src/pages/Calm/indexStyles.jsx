d20f763a3fea1780f625609c334baf85faad4b34
joelpavlovsky <joel.pavlovsky@gmail.com>
2022-05-11 14:58:14 +0300

    asd

---
+++ my-app/src/pages/Calm/indexStyles.jsx
@@ -0,0 +1,11 @@
+import { Theme } from '@material-ui/core/styles';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+export const useStyles = makeStyles(theme => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+}));
