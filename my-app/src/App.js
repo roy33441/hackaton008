c4b0ec186d3f1e26869a82bfb35e5eac8d33e472
Roy Canani <roycanani@Roys-MacBook-Pro.local>
2022-05-11 14:45:50 +0300

    Added routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import "./App.css";
+import Register from "./pages/Register";
+import Calm from "./pages/Calm";
 
 function App() {
   return (
@@ -19,6 +23,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" component={Home} />
+          <Route path="/register" component={Register} />
+          <Route path="/calm" component={Calm} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ my-app/src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default (
+    <div>
+        <button>
+            click me
+        </button>
+    </div>
+);
