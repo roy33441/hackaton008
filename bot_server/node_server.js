32b97d5ff74ddee7e0948011d1e5202fdc122b69
joelpavlovsky <joel.pavlovsky@gmail.com>
2022-05-12 00:44:30 +0300

    initilize project

--- bot_server/node_server.js
+++ bot_server/node_server.js
@@ -1,9 +1,16 @@
-const http = require("http");
+// const http = require("http");
+const path = require('path');
 
 const hostname = "127.0.0.1";
 const port = 5000;
 
-const server = http.createServer((req, res) => {
+const express = require('express');
+const app = express();
+
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use('/', express.static(publicPath));
+app.use('/api/mail', (req, res) => {
   var nodemailer = require("nodemailer");
   var smtpTransport = require("nodemailer-smtp-transport");
 
@@ -35,8 +42,42 @@ const server = http.createServer((req, res) => {
       res.end('Hello World');
     }
   });
-});
+})
 
-server.listen(port, hostname, () => {
+// const server = http.createServer((req, res) => {
+  // var nodemailer = require("nodemailer");
+  // var smtpTransport = require("nodemailer-smtp-transport");
+
+  // var transporter = nodemailer.createTransport(
+  //   smtpTransport({
+  //     service: "gmail",
+  //     host: "smtp.gmail.com",
+  //     auth: {
+  //       user: "hac.hashlama@gmail.com",
+  //       pass: "alintheloser123456",
+  //     },
+  //   })
+  // );
+
+  // var mailOptions = {
+  //   from: "hac.hashlama@gmail.com",
+  //   to: "joel.pavlovsky@gmail.com",
+  //   subject: "Sending Email using Node.js",
+  //   text: "בדוק את האוטיסט שלך",
+  // };
+
+  // transporter.sendMail(mailOptions, function (error, info) {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log("Email sent: " + info.response);
+  //     res.statusCode = 200;
+  //     res.setHeader('Content-Type', 'text/plain');
+  //     res.end('Hello World');
+  //   }
+  // });
+// });
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
