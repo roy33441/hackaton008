664575cec4f277be5e049deb3634c53e2702d072
Roy Canani <roycanani@Roys-MacBook-Pro.local>
2022-05-12 05:02:46 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
 import Register from "./pages/Register";
@@ -15,24 +15,34 @@ const useStyles = makeStyles({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    height:'100vh'
+    height: "100vh",
   },
   photo: {
-    height: '25vh',
-    width: '45vh'
-  }
+    height: "25vh",
+    width: "45vh",
+  },
 });
 function App() {
   const classes = useStyles();
-  const [ openDell, setOpenDell ] = React.useState(true)
+  const navigate = useNavigate();
+  const [openDell, setOpenDell] = React.useState(true);
   useEffect(() => {
     const timer = setTimeout(() => setOpenDell(false), 2000);
+    if (
+      !localStorage.getItem("phoneNumber") ||
+      !localStorage.getItem("name") ||
+      !localStorage.getItem("color")
+    ) {
+      navigate("/register");
+    }
   }, []);
 
   if (openDell) {
-    return     <div className={classes.photoContainer}>
-      <img className={classes.photo}  src='photos/dell-logo.jpg' />
-    </div>;
+    return (
+      <div className={classes.photoContainer}>
+        <img className={classes.photo} src="photos/dell-logo.jpg" />
+      </div>
+    );
   }
 
   return (
